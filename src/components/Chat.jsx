18f1f8cd914d8bcdb022677aeb19b39dcc181a3f
raj-saroj-vst-au4 <raj-saroj-vst-au4>
2023-07-31 20:31:03 +0530

    removed waiting msg on change

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -58,17 +58,19 @@ const Chat = ({ socket, mySocketId, remoteUser }) => {
         className="flex flex-col flex-grow h-0 p-4 overflow-auto"
         key={Math.random() * 3}
       >
-        <div className="flex w-full mt-2 space-x-3 max-w-xs">
-          <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-          <div>
-            <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-              <p className="text-sm">Waiting for someone to Connect.</p>
+        {!remoteUser && (
+          <div className="flex w-full mt-2 space-x-3 max-w-xs">
+            <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+            <div>
+              <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                <p className="text-sm">Waiting for someone to Connect.</p>
+              </div>
+              <span className="text-xs text-gray-500 leading-none">
+                2 min ago
+              </span>
             </div>
-            <span className="text-xs text-gray-500 leading-none">
-              2 min ago
-            </span>
           </div>
-        </div>
+        )}
 
         {textRecords.map((msg, index) => {
           return msg.from === "me" ? (
