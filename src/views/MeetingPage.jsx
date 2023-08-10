9b5989a31615dcb4644e75d6a5c32baa4e67aa42
raj-saroj-vst-au4 <raj-saroj-vst-au4>
2023-08-11 00:35:26 +0530

    re~initiate backend if down

--- src/views/MeetingPage.jsx
+++ src/views/MeetingPage.jsx
@@ -4,6 +4,7 @@ import Peer from "peerjs";
 import OnlineList from "../components/OnlineList";
 import Chat from "../components/Chat";
 import Modal from "../components/Modal";
+import Loader from "../components/Loader";
 
 const socket = io.connect("https://interviewconnect-backend-4a27.onrender.com");
 // const socket = io.connect("http://localhost:8000");
@@ -18,10 +19,40 @@ const MeetingPage = () => {
   const [remPeer, setRemPeer] = useState();
   const [remoteUser, setRemoteUser] = useState("");
   const [callStatus, setCallStatus] = useState(false);
+  const [backendReady, setBackendReady] = useState(false);
+  const [retries, setRetries] = useState(0);
+  const maxRetries = 30;
+  const retryInterval = 2000;
 
   const myVid = useRef();
   const remVid = useRef();
   const peerInstance = useRef();
+
+  const checkBackendStatus = async () => {
+    if (!backendReady) {
+      try {
+        const response = await fetch(
+          "https://interviewconnect-backend-4a27.onrender.com"
+        );
+        if (response.status === 200) {
+          setBackendReady(true);
+          console.log("Backend is up and running!");
+        } else {
+          console.log(
+            `Backend not yet ready. Retrying in ${
+              retryInterval / 1000
+            } seconds...`
+          );
+          setTimeout(checkBackendStatus, retryInterval);
+          setRetries((prevRetries) => prevRetries + 1);
+        }
+      } catch (error) {
+        console.error("Error while checking backend status:", error);
+        setTimeout(checkBackendStatus, retryInterval);
+        setRetries((prevRetries) => prevRetries + 1);
+      }
+    }
+  };
 
   useEffect(() => {
     socket.on("me", (id) => {
@@ -46,6 +77,10 @@ const MeetingPage = () => {
 
     peerInstance.current = peer;
   }, []);
+
+  useEffect(() => {
+    checkBackendStatus();
+  }, [backendReady, retries]);
 
   useEffect(() => {
     if (myVid.current) {
@@ -106,82 +141,94 @@ const MeetingPage = () => {
   };
 
   return (
-    <div className="grid grid-cols-5 gap-2 bg-gray-100 h-fit">
-      <div className="modal absolute">
-        <Modal
-          socket={socket}
-          toggleModal={toggleModal}
-          hideModal={hideModal}
-          modalData={modalData}
-          setRemoteUser={setRemoteUser}
-          remoteUser={remoteUser}
-          myPeer={myPeer}
-          setRemPeer={setRemPeer}
-        />
-      </div>
+    <div>
+      {mySocketId ? (
+        <div className="grid grid-cols-5 gap-2 bg-gray-100 h-fit">
+          <div className="modal absolute">
+            <Modal
+              socket={socket}
+              toggleModal={toggleModal}
+              hideModal={hideModal}
+              modalData={modalData}
+              setRemoteUser={setRemoteUser}
+              remoteUser={remoteUser}
+              myPeer={myPeer}
+              setRemPeer={setRemPeer}
+            />
+          </div>
 
-      <div className="col-span-1">
-        <OnlineList
-          socket={socket}
-          mySocketId={mySocketId}
-          setRemoteUser={setRemoteUser}
-          myPeer={myPeer}
-          setRemPeer={setRemPeer}
-        />
-      </div>
-      <div className="col-span-3">
-        <div className="sticky bg-gray-800 rounded-xl p-3 w-1200 h-800 m-5 shadow-xl">
-          <div className="absolute inline-block text-white top-5 left-5">
-            <br></br>
-            My Socket: {mySocketId}
-            <br></br>
-            Rem Socket : {remoteUser}
-            <br></br>
+          <div className="col-span-1">
+            <OnlineList
+              socket={socket}
+              mySocketId={mySocketId}
+              setRemoteUser={setRemoteUser}
+              myPeer={myPeer}
+              setRemPeer={setRemPeer}
+            />
           </div>
-          {callStatus ? (
-            <video playsInline muted ref={remVid} width="1100px" autoPlay />
-          ) : (
-            <img src="https://picsum.photos/1100/800" alt="" loading="lazy" />
-          )}
-          <div className="absolute rounded-lg bottom-5 right-5 w-100 h-100 shadow-xl overflow-hidden">
-            {myStream ? (
-              <video playsInline muted ref={myVid} width="200px" autoPlay />
-            ) : (
-              <img src="https://i.pravatar.cc/250" alt="" loading="lazy" />
-            )}
+          <div className="col-span-3">
+            <div className="sticky bg-gray-800 rounded-xl p-3 w-1200 h-800 m-5 shadow-xl">
+              <div className="absolute inline-block text-white top-5 left-5">
+                <br></br>
+                My Socket: {mySocketId}
+                <br></br>
+                Rem Socket : {remoteUser}
+                <br></br>
+              </div>
+              {callStatus ? (
+                <video playsInline muted ref={remVid} width="1100px" autoPlay />
+              ) : (
+                <img
+                  src="https://picsum.photos/1100/800"
+                  alt=""
+                  loading="lazy"
+                />
+              )}
+              <div className="absolute rounded-lg bottom-5 right-5 w-100 h-100 shadow-xl overflow-hidden">
+                {myStream ? (
+                  <video playsInline muted ref={myVid} width="200px" autoPlay />
+                ) : (
+                  <img src="https://i.pravatar.cc/250" alt="" loading="lazy" />
+                )}
+              </div>
+              <div className="text-white text-center absolute bottom-5 left-10">
+                {callStatus ? (
+                  <button
+                    type="button"
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded"
+                    onClick={() => {
+                      leaveCall();
+                      socket.emit("vcEnd", remoteUser);
+                    }}
+                  >
+                    End Call
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                    onClick={handleCallUser}
+                  >
+                    Start VideoCall
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
-          <div className="text-white text-center absolute bottom-5 left-10">
-            {callStatus ? (
-              <button
-                type="button"
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded"
-                onClick={() => {
-                  leaveCall();
-                  socket.emit("vcEnd", remoteUser);
-                }}
-              >
-                End Call
-              </button>
-            ) : (
-              <button
-                type="button"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                onClick={handleCallUser}
-              >
-                Start VideoCall
-              </button>
-            )}
+          <div className="flex flex-col col-span-1 items-center justify-center text-gray-800 pt-5 pb-5 h-full">
+            <Chat
+              socket={socket}
+              mySocketId={mySocketId}
+              remoteUser={remoteUser}
+              setRemoteUser={setRemoteUser}
+            />
           </div>
         </div>
-      </div>
-      <div className="flex flex-col col-span-1 items-center justify-center text-gray-800 pt-5 pb-5 h-full">
-        <Chat
-          socket={socket}
-          mySocketId={mySocketId}
-          remoteUser={remoteUser}
-          setRemoteUser={setRemoteUser}
-        />
-      </div>
+      ) : (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-100">
+          <Loader />
+        </div>
+      )}
     </div>
   );
 };
